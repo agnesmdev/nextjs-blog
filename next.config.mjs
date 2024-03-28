5773c032375e57900c20a2f4cec3091f684b8c15
agnesmdev <agnesm.dev@gmail.com>
2024-03-28 15:59:20 +0100

    Remove path and prefix to move to main page

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/nextjs-blog",
-    assetPrefix: "https://agnesmdev.github.io/nextjs-blog",
     output: "export",
     reactStrictMode: true,
     images: {
